fe5be972f5cb156f2bb4bb1ed1b1b1857e2b4851
Jyothis V <jyothisv@gmail.com>
2018-10-01 11:00:00 +0530

    Add game_ends and evaluator functions.

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
+import random as rnd
 
 # A class for raising invalid move exceptions
 class InvalidMove(Exception):
@@ -15,8 +16,9 @@ class Breakthrough:
         self.board[:2, :] = 2
         self.board[board_size-2:, :] = 1
 
-        # Number of pieces captured so far.
-        self.captured = [0, 0]
+        # Number of pieces captured so far by each player.
+        # The first element is just for padding.
+        self.captured = [0, 0, 0]
 
         self.player = None      # The player whose turn it is now.
 
@@ -84,13 +86,11 @@ class Breakthrough:
         # Also, the to_pos would now hold the player's piece.
         self.board[to_pos] = self.player
 
-        capture = 0
+        capture = False
         if move_type == 2:
-            # Diagonal move
-
-            # Captured player
-            capture = next_player(self.player)
-            self.captured[capture - 1] += 1
+            capture = True
+            # Number of pieces that self.player captures is incremented.
+            self.captured[self.player] += 1
 
         return self.game_ends(), capture
 
@@ -98,4 +98,57 @@ class Breakthrough:
     def game_ends(self):
         """If the game has ended, return the winner's id (1 or 2).
         If the game has ended in a draw, return 0. Otherwise, return None.
+        A player wins if one of his/her pieces reaches the opposite end.
+        Another way for a player to win is to capture all the pieces of the enemy.
         """
+        if self.captured[self.player] == 2*self.board_size:
+            return True
+
+        target_row = 0
+        if self.player == 2:
+            target_row = self.board_size-1
+
+        return sum( self.board[target_row, :] == self.player ) >= 1
+
+
+    def evaluator(self, player1, player2, trials=5):
+        """Play the game using two agents. The agents are expected to be the objects of
+        the Player class. Scoring: if a player returns an invalid move, or
+        exceeds the time limit, his/her will get a 0 points and any of his/her
+        previous wins will be forgotten. In that case, the opponent will get 1 point for the game.
+        In a normal game, the winner will get 2 points per game.
+        """
+
+        games = ( [True] * trials ) + ([False] * trials)
+        rnd.shuffle(games)
+
+        scores = [0, 0, 0]
+
+        pl = [None, None, None]
+
+        for game in games:
+            if game:
+                pl[1], pl[2] = player1, player2
+            else:
+                pl[1], pl[2] = player2, player1
+
+            pl[1].start(1)        # First player
+            pl[2].start(2)        # Second player
+
+            self.player = 1
+            capture = 0
+            move = None
+            while True:
+                move = pl[self.player].next_move(move, capture)
+                ends, capture = self.do_move(move)
+                if ends:
+                    # Let both players know that the game has ended.
+                    pl[1].finish(self.player, move)
+                    pl[2].finish(self.player, move)
+                    break
+                else:
+                    self.player = self.next_player()
+
+            # At this point, presumably we know who won. Update the scores.
+            scores[self.player] += 2
+        return scores
